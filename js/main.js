ffec4ff1d9fe378d5beb48d09f152c5fbd9042df
dario.rodriguez_feni <dario.rodriguez@fenicio.io>
2024-08-27 19:25:17 -0300

    primer commit

--- js/main.js
+++ js/main.js
@@ -10,5 +10,6 @@ while(ubicacion > 19) {
     ubicacion = parseInt(prompt("Valor incorrecto, debe ingresar un numero entre 1 y 19: \n1 - Montevideo \n2 - Canelones\n3 - Maldonado\n4 - Rocha\n5 - Treinta y Tres\n6 - Lavalleja\n7 - Florida\n8 - Durazno\n9 - Cerro Largo\n10 - Tacuarembó\n11 - Rivera\n12 - Artigas\n13 - Salto\n14 - Paysandú\n15 - Río Negro\n16 - Soriano\n17 - Colonia\n18 - San José\n19 - Flores"));
 }
 
+let salario = parseInt(prompt("Ingrese su salario liquido"));
 
 
